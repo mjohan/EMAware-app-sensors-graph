ae9a3b5eee743bffb76f1024a9c13331311ef2fd
Muhammad Johan Alibasa <m.johan.alibasa@gmail.com>
2017-02-10 17:34:44 +1100

    update home dashboard page and include lodash

--- src/app/sensor-chart/sensor-chart.component.ts
+++ src/app/sensor-chart/sensor-chart.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { ChartModule } from 'angular2-highcharts';
 
+import * as _ from 'lodash';
+
 @Component({
   selector: 'sensor-chart',
   templateUrl: './sensor-chart.component.html',
@@ -13,25 +15,25 @@ export class SensorChartComponent implements OnInit {
 
   constructor() {
     this.options = {
-      title: { text: 'Dummy Chart' },
-      chart: { zoomType: 'x'},
+      chart: { zoomType: 'x', width: window.screen.width/2, height: 450 },
       plotOptions: { series: { showInNavigator: true } },
       xAxis: { type: 'datetime' },
       rangeSelector: { selected: 4 },
-      series: [{ 
-        name: 'Sensor A', 
+      series: [{
+        name: 'Sensor A',
+        type: 'spline',
         data: [
-          [1486664737234, 23.21], 
-          [1486664837234, 54.12], 
-          [1486664937234, 76.12], 
-          [1486665037234, 12.23], 
-          [1486665137234, 43.12], 
-          [1486665237234, 23.21], 
-          [1486665337234, 54.12], 
-          [1486665437234, 76.12], 
-          [1486665537234, 12.23], 
+          [1486664737234, 23.21],
+          [1486664837234, 54.12],
+          [1486664937234, 76.12],
+          [1486665037234, 12.23],
+          [1486665137234, 43.12],
+          [1486665237234, 23.21],
+          [1486665337234, 54.12],
+          [1486665437234, 76.12],
+          [1486665537234, 12.23],
           [1486665637234, 43.12]
-        ] 
+        ]
       }]
     };
   }
